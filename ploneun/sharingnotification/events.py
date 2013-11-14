a5bcd7ecfce9a5d328656e949d7705d1a5a41bda
Izhar Firdaus <izhar@inigo-tech.com>
2013-11-14 16:29:18 +0800

    implement IObjectSharingModifiedEvent on the event object (Re #902)

--- ploneun/sharingnotification/events.py
+++ ploneun/sharingnotification/events.py
@@ -1,8 +1,10 @@
 from zope.component.interfaces import ObjectEvent
-
+from zope.interface import implements
+from ploneun.sharingnotification.interfaces import IObjectSharingModifiedEvent
 
 class ObjectSharingModifiedEvent(ObjectEvent):
-    
+    implements(IObjectSharingModifiedEvent)
+
     def __init__(self, object, user_roles):
         super(ObjectSharingModifiedEvent, self).__init__(object)
         self.user_roles = user_roles
